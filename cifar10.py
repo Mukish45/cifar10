a1437c6e8fb7fcf2347d91bedf5c1c7ee9dbb938
Mukish S <73186875+Mukish45@users.noreply.github.com>
2022-10-26 22:59:11 +0530

    Add files via upload

---
+++ cifar10.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import cv2
+import cv2
+import numpy as np
+from tensorflow import keras
+model = keras.models.load_model('/Users/mukish/Documents/Sem-5/Advanced Machine Learning Lab/Deployment/Mymodel.h5')
+
+upload_image = st.file_uploader(label='Upload image', type=['png', 'jpg','jpeg'], accept_multiple_files=False)
+img = cv2.resize(upload_image, dsize=(32, 32))
+img = np.reshape(img,[1,32,32,3])
+y_predict = np.argmax(model.predict(img), axis=1)
+if upload_image is not None:
+    st.text(y_predict)
+
+

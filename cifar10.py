4c40f1325d0ec3ea0b662c4edaecb0bdb47cd88b
Mukish S <73186875+Mukish45@users.noreply.github.com>
2022-10-27 11:45:44 +0530

    Update cifar10.py

--- cifar10.py
+++ cifar10.py
@@ -1,15 +1,18 @@
 import streamlit as st
 import cv2
-import cv2
 import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
 from tensorflow import keras
-model = keras.models.load_model('Mymodel.h5')
+model = keras.models.load_model('/Users/mukish/Documents/Sem-5/Advanced Machine Learning Lab/Deployment/Mymodel.h5')
 
 upload_image = st.file_uploader(label='Upload image', type=['png', 'jpg','jpeg'], accept_multiple_files=False)
-img = cv2.resize(upload_image, dsize=(32, 32))
-img = np.reshape(img,[1,32,32,3])
-y_predict = np.argmax(model.predict(img), axis=1)
 if upload_image is not None:
-    st.text(y_predict)
+	image = Image.open(upload_image)
+	converted_img = np.array(image.convert('RGB'))
+	img = cv2.resize(converted_img, dsize=(32, 32))
+	img_reshape = np.reshape(img,[1,32,32,3])
+	y_predict = np.argmax(model.predict(img_reshape), axis=1)
+	st.text(y_predict)
 
 
